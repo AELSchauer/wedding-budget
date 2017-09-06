a1c21482ca33405b4a62dd9e004562edd9a8070e
Ashley Schauer <aelschauer@gmail.com>
2017-09-06 17:35:54 -0500

    add static pages and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # root 'static_pages#index'
+  root 'static_pages#index'
 
-  # get 'login', to: 'sessions#new'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
