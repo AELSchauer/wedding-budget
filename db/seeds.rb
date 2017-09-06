ae7ce898dbc6aee4aef183c3218ff1bfcb45b3f1
Ashley Schauer <aelschauer@gmail.com>
2017-09-06 18:04:34 -0500

    add login and logout functionality with tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ User.create(
   first_name: "Alex",
   last_name: "Ant",
   email: "alex.ant@email.com",
-  password: "test",
-  password_confirmation: "test",
+  password: "password",
+  password_confirmation: "password",
 )
 
 User.create(
   first_name: "Barry",
   last_name: "Buffalo",
   email: "barry.buffalo@email.com",
-  password: "test",
-  password_confirmation: "test",
+  password: "password",
+  password_confirmation: "password",
 )
